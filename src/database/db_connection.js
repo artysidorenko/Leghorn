06ffb9512aab3ccdbf713e0215757162af59647d
Arty <26905074+artysidorenko@users.noreply.github.com>
2019-02-09 22:33:19 +0000

    configured remote heroku database

--- src/database/db_connection.js
+++ src/database/db_connection.js
@@ -2,24 +2,34 @@ const { Pool } = require('pg');
 const url = require('url');
 require('env2')('./config.env');
 
-let URL_DB = process.env.DB_URL;
+let options = {};
 
-if (process.env.NODE_ENV === 'test') {
-  URL_DB = process.env.TEST_DB_URL;
+if (process.env.DATABASE_URL) {
+  // database linked to heroku account
+  options = {
+    connectionString: process.env.DATABASE_URL,
+    ssl: true,
+  };
+} else {
+  // database being run locally
+  let URL_DB = process.env.DB_URL;
+
+  if (process.env.NODE_ENV === 'test') {
+    URL_DB = process.env.TEST_DB_URL;
+  }
+
+  if (!URL_DB) throw new Error('Enviromental variable DB_URL must be set.');
+
+  const params = url.parse(URL_DB);
+  const [username, password] = params.auth.split(':');
+  options = {
+    host: params.hostname,
+    port: params.port,
+    database: params.pathname.split('/')[1],
+    max: process.env.DB_MAX_CONNECTIONS || 2,
+    user: username,
+    password,
+  };
 }
-
-if (!URL_DB) throw new Error('Enviromental variable DB_URL must be set.');
-
-const params = url.parse(URL_DB);
-const [username, password] = params.auth.split(':');
-const options = {
-  host: params.hostname,
-  port: params.port,
-  database: params.pathname.split('/')[1],
-  max: process.env.DB_MAX_CONNECTIONS || 2,
-  user: username,
-  password,
-};
-
 
 module.exports = new Pool(options);
